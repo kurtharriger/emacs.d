5cb5e29a4753a9f25ef3516aca45e989f2ae112d
Kurt Harriger <kurt.harriger@twcable.com>
2011-05-17 10:15:04 -0600

    rebind %W to delete-frame

--- kurtharriger.el
+++ kurtharriger.el
@@ -4,8 +4,5 @@
 
 (global-set-key (kbd "\C-x\C-c") 'save-buffers-kill-emacs)
 
-(defun kill-current-buffer ()
-  (interactive)
-  (kill-buffer (current-buffer)))
-(global-set-key (kbd "s-w") 'kill-current-buffer)
+(global-set-key (kbd "s-w") 'delete-frame)
 
