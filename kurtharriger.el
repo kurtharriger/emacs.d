28babefac6de56233f9fc89694c2291410e5f509
Kurt Harriger <kurt.harriger@twcable.com>
2011-06-04 15:27:12 -0600

    various fixes for frame config issues when started via daemon

--- kurtharriger.el
+++ kurtharriger.el
@@ -3,29 +3,87 @@
       (normal-top-level-add-to-load-path '("."))
       (normal-top-level-add-subdirs-to-load-path))
 
-;; Windowed
-(when window-system 
-  (require 'alpha)
-  (require 'color-theme-ir-black)
-  (modify-frame-parameters (selected-frame) '((alpha . 85)))
-  (color-theme-ir-black)
-  ;;  (global-set-key (kbd "\C-x\C-c") 'save-buffers-kill-emacs)
-  (global-unset-key (kbd "\C-x\C-c"))
-  (global-set-key (kbd "s-w") 'delete-frame)
-  (global-set-key (kbd "s-K") 'server-edit)
-  (server-start))
+(defvar after-make-console-frame-hooks '()
+"Hooks to run after creating a new TTY frame")
+(defvar after-make-window-system-frame-hooks '()
+"Hooks to run after creating a new window-system frame")
 
-;; MouseTerm
- 
+(defun run-after-make-frame-hooks (frame)
+"Selectively run either `after-make-console-frame-hooks' or
+`after-make-window-system-frame-hooks'"
+  (select-frame frame)
+  (run-hooks (if window-system
+               'after-make-window-system-frame-hooks
+               'after-make-console-frame-hooks)))
 
-(unless window-system
-  (xterm-mouse-mode 1)
-  (global-set-key [mouse-4] '(lambda ()
-                               (interactive)
-                               (scroll-down 1)))
-  (global-set-key [mouse-5] '(lambda ()
-                               (interactive)
-                               (scroll-up 1))))
+(add-hook 'after-make-frame-functions 'run-after-make-frame-hooks)
+(add-hook 'after-init-hook (lambda ()
+  (run-after-make-frame-hooks (selected-frame))))
 
+
+(setq color-theme-is-global nil)
+
+(add-hook 'after-make-window-system-frame-hooks
+          '(lambda ()
+             (require 'alpha)
+             (require 'color-theme-ir-black)
+             (modify-frame-parameters (selected-frame) '((alpha . 85)))
+             (color-theme-ir-black)
+             ;;  (global-set-key (kbd "\C-x\C-c") 'save-buffers-kill-emacs)
+             ;; (global-unset-key (kbd "\C-x\C-c"))
+             ;; (global-set-key (kbd "s-w") 'delete-frame)
+             ;; (global-set-key (kbd "s-K") 'server-edit)
+             ))
+
+(add-hook 'after-make-console-system-frame-hooks
+          '(lambda ()
+             (xterm-mouse-mode 1)
+             (global-set-key [mouse-4] '(lambda ()
+                                          (interactive)
+                                          (scroll-down 1)))
+             (global-set-key [mouse-5] '(lambda ()
+                                          (interactive)
+                                          (scroll-up 1)))))
 
 (require 'groovy-mode)
+
+
+(defun my-quit ()
+  (interactive)
+  (if (server-clients)
+      (do 
+          (if server-buffer-clients (server-edit))
+          (make-frame-invisible nil t))
+      (save-buffers-kill-emacs)))
+
+(defun my-kill ()
+  (interactive)
+   (if server-buffer-clients       
+     (server-edit)
+     (ido-kill-buffer)))
+
+(defun my-kill-frame ()
+  (interactive)
+  (if server-clients
+      (do
+          (if server-buffer-clients (server-edit))
+          (make-frame-invisible nil t))
+      (delete-this-frame)))
+
+
+(global-set-key (kbd "C-x C-c") 'my-quit)
+;;(global-set-key (kbd "s-q") 'my-quit)
+;;(global-set-key (kbd "C-x 5 0") 'my-kill-frame)
+;;(global-set-key (kbd "s-w") 'my-kill-frame)
+
+(add-hook 'server-switch-hook 
+  (lambda ()
+    (local-set-key (kbd "C-x k") 'my-kill))
+                   
+
+(unless (and (functionp 'server-running-p)  (server-running-p))
+   (server-start))
+
+
+
+
