c45aea5eb9b92b39551e133827775b139db3cf99
Kurt Harriger <kurtharriger@gmail.com>
2011-06-15 19:11:50 -0600

    add alpha package

---
+++ elpa/alpha-1.0/alpha-autoloads.el
@@ -0,0 +1,18 @@
+;;; alpha-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("alpha-pkg.el" "alpha.el") (19961 21431
+;;;;;;  270068))
+
+;;;***
+
+(provide 'alpha-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; alpha-autoloads.el ends here
